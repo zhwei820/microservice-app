f942e8d314ab6a9c3883989daf2dd6248c0868bd
zhouwei <zhouwei@dianjoy.com>
2016-12-16 21:22:30 +0800

    .

--- docker/Dockerfile.profile
+++ docker/Dockerfile.profile
@@ -1,5 +1,5 @@
 FROM centos
-ADD https://github.com/buptmiao/microservice-app/releases/download/v1.0.1/microservice-app-v1.0.1-linux-amd64.tar.gz .
+ADD http://localhost:8000/microservice-app-v1.0.1-linux-amd64.tar.gz .
 RUN tar -xzf microservice-app-v1.0.1-linux-amd64.tar.gz -C .
 
 EXPOSE 8083 6063
--- vagrant/provision.sh
+++ vagrant/provision.sh
@@ -9,7 +9,7 @@ if [ `hostname` == "node-0" ];then
     sudo cp -a etcd-v3.0.0-linux-amd64/{etcd,etcdctl} /usr/local/bin/
 else
     echo "Installing micro binaries..."
-    [ ! -f "microservice-app-v1.0.1-linux-amd64.tar.gz" ] && wget -q https://github.com/buptmiao/microservice-app/releases/download/v1.0.1/microservice-app-v1.0.1-linux-amd64.tar.gz
+    [ ! -f "microservice-app-v1.0.1-linux-amd64.tar.gz" ] && wget -q http://localhost:8000/microservice-app-v1.0.1-linux-amd64.tar.gz
     sudo tar -xzf microservice-app-v1.0.1-linux-amd64.tar.gz -C .
     sudo cp -a microservice-app-v1.0.1-linux-amd64/* /usr/local/bin/
 fi
